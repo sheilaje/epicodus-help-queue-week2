15ff73ea5d5ca20e2691886e7879c06d046f8633
Student <example@example.com>
2019-02-11 11:56:20 -0800

    Enviornment set up

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Ticket(props){
+  let ticketStyles = {
+    backgroundColor:'ecf0f1',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px',
+    color: "red"
+  }
+  return(
+    <div style={ticketStyles}>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
+}
+
+export default Ticket;
---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(){
+  return(
+    <Ticket
+      location= "3A"
+      names="Jane"
+      issue="Firebase will not save record!"/>
+  );
+}
+
+export default TicketList;
