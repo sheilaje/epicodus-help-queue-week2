f4cc28a594f2b0eaa7b4eef7b807b94c2ed821cc
Student <example@example.com>
2019-02-19 16:42:23 -0800

    add moment.js to update time continuously

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Moment from 'moment'
 
 const styles = theme => ({
   root: {
@@ -32,6 +33,7 @@ const { classes } = props;
         <Grid item xs>
         <Paper className={classes.paper}>
         <h3>{props.location} - {props.names}</h3>
+        <h4>{displayTimeOpen(props.timeOpen)}</h4>
         <p><em>{props.issue}</em></p>
         <p>{props.id}</p>
         <hr/>
@@ -43,11 +45,16 @@ const { classes } = props;
   )
 }
 
+function displayTimeOpen(timeOpen){
+  return timeOpen.from(new Moment(),true)
+}
+
 Ticket.propTypes ={
   classes: PropTypes.object.isRequired,
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  issue: PropTypes.string
+  issue: PropTypes.string,
+  timeOpen: PropTypes.instanceOf(Moment).isRequired
 }
 
 export default withStyles(styles)(Ticket);
